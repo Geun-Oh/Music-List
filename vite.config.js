532474d6d66e8ca6ed64f1a0778dabfa6cba49dc
ohyeonggeun-hag <>
2022-02-27 14:30:08 +0000

    cmd

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
  * @type { import('vite').UserConfig }
  */
 export default {
+  base: "/Music-List/",
   plugins: [reactRefresh()],
   server: {
     host: '0.0.0.0',
